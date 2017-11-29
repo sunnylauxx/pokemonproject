93f4619af46a09b4ff9aad36939d28d4cdc35e2e
Sunny Lau <31958667+sunnylauxx@users.noreply.github.com>
2017-11-29 14:26:16 -0500

    project

---
+++ pokemonproject.py
@@ -0,0 +1,6 @@
+person = input ("Enter your name: ")
+
+print(person,'Welcome to Pokemon World! When you catch one pokemon successfully, you will earn 5 credits. In room one, you can catch grass pokemons. when you had earned 20 credits in room one then you can go to room two and catch fire pokemons. Again, when you had earned 30 points in room two, then you can go to room three and catch all types of pokemons! Enjoy!' )
+
+
+
